8f246532c4f754b4ec71a24b232b9a28d61dd58a
hamza4213 <hamzashabbir4213@gmail.com>
2022-01-04 12:21:50 +0500

    firebase issue resolved

--- Firebase.js
+++ Firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCRmD35HRTAkV8Du4bCyMYePSEl0KLcjls",
   authDomain: "foodapp-6c700.firebaseapp.com",
@@ -13,16 +9,18 @@ const firebaseConfig = {
   storageBucket: "foodapp-6c700.appspot.com",
   messagingSenderId: "35625101857",
   appId: "1:35625101857:web:6db9b451d526fdc59c9160",
-  measurementId: "G-JYYG2Y55CL"
+  measurementId: "G-JYYG2Y55CL",
 };
 
-// Initialize Firebase
 let app;
+
 if (firebase.apps.length === 0) {
-    app = firebase.initializeApp(firebaseConfig)
+  app = firebase.initializeApp(firebaseConfig);
 } else {
-    app = firebase.app()
+  app = firebase.app();
 }
 
-const auth = firebase.auth()
-export {auth}
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
